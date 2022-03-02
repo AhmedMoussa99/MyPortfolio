b1104e80b97db49a8ca76f59cbf5e382c199a6ef
Ahmed Moussa <01093938149a@gmail.com>
2022-03-02 05:16:21 -0800

    Add project files.

---
+++ Infrastructure/Migrations/20201224160911_Initialcreate.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class Initialcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Street = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Number = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PortfolioItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false, defaultValueSql: "NEWID()"),
+                    ProjectName = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PortfolioItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Owners",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false, defaultValueSql: "NEWID()"),
+                    FullName = table.Column<string>(nullable: true),
+                    profile = table.Column<string>(nullable: true),
+                    Avatar = table.Column<string>(nullable: true),
+                    AddressId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owners", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Owners_Address_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Address",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Owners",
+                columns: new[] { "Id", "AddressId", "Avatar", "FullName", "profile" },
+                values: new object[] { new Guid("20f97cb9-793b-4687-889d-5b04cb91cccf"), null, "avatar.jpg", "Ahmed Moussa", "Microsoft .Net Web Developer" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Owners_AddressId",
+                table: "Owners",
+                column: "AddressId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Owners");
+
+            migrationBuilder.DropTable(
+                name: "PortfolioItems");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+        }
+    }
+}
---
+++ MyPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.ViewModels;
+
+namespace MyPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IUnitOfWork<Owner> _owner;
+        private readonly IUnitOfWork<PortfolioItem> _portfolioitem;
+
+        public HomeController(IUnitOfWork<Owner> Owner , IUnitOfWork<PortfolioItem> portfolioitem)
+        {
+            _owner = Owner;
+            _portfolioitem = portfolioitem;
+        }
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                Owner = _owner.Entity.GetAll().First(),
+                portfolioItems = _portfolioitem.Entity.GetAll().ToList()
+            };
+            return View(homeViewModel);
+        }
+    }
+}
